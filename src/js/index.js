e0b39db3832a0f541cc42922aac89ce1c7fde083
jicho <jIcho6412@gmail.com>
2024-04-15 22:13:31 +0900

    11. 메뉴 수정하기 - 수정 버튼을 눌러 'prompt' 창에서 메뉴 수정 | 브라우저 인터페이스
    '이벤트 위임'의 개념, closest(), prompt()

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,7 @@
 
 // TODO 메뉴 추가
 // - [x] 메뉴의 이름을 입력 받고 엔터키 입력으로 추가한다.
-// - [ ] 메뉴의 이름을 입력 받고 확인 버튼을 클릭하면 메뉴를 추가한다.
+// - [x] 메뉴의 이름을 입력 받고 확인 버튼을 클릭하면 메뉴를 추가한다.
 // - [x] 추가되는 메뉴의 아래 마크업은 <ul id="espresso-menu-list" class="mt-3 pl-0"></ul> 안에 삽입해야 한다.
 // - [x] 총 메뉴 갯수를 count하여 상단에 보여준다.
 // - [x] 메뉴가 추가되고 나면, input은 빈 값으로 초기화한다.
@@ -11,6 +11,24 @@
 const $ = (selector) => document.querySelector(selector);
 
 function App() {
+  // TODO 메뉴 수정
+  // - [ ] 메뉴의 수정 버튼클릭 이벤트를 받고, 메뉴를 수정하는 모달창이 뜬다.(브라우저에서 제공하는 prompt 인터페이스를 활용)
+  // - [ ] 모달창에서 신규 메뉴명을 입력 받고, 확인 버튼을 누르면 메뉴가 수정된다.
+
+  $("#espresso-menu-list").addEventListener("click", (e) => {
+    if (e.target.classList.contains("menu-edit-button")) {
+      // console.log(e.target);
+      const $menuName = e.target.closest("li").querySelector(".menu-name");
+      // const menuName = $menuName.innerText;
+      // 굳이 변수에 담지않고 바로 불러오게끔 해도 될 것 같다.
+      const updatedMenuName = prompt(
+        "메뉴명을 수정하세요",
+        $menuName.innerText
+      );
+      $menuName.innerText = updatedMenuName;
+    }
+  });
+
   $("#espresso-menu-form").addEventListener("submit", (e) => {
     e.preventDefault();
   });
@@ -42,7 +60,7 @@ function App() {
         </li>`;
     };
 
-    console.log(menuItemTemplate(espressoMenuName));
+    // console.log(menuItemTemplate(espressoMenuName));
 
     $("#espresso-menu-list").insertAdjacentHTML(
       "beforeend",
@@ -72,10 +90,6 @@ function App() {
 }
 
 App();
-
-// TODO 메뉴 수정
-// - [ ] 메뉴의 수정 버튼클릭 이벤트를 받고, 메뉴를 수정하는 모달창이 뜬다.(브라우저에서 제공하는 prompt 인터페이스를 활용)
-// - [ ] 모달창에서 신규 메뉴명을 입력 받고, 확인 버튼을 누르면 메뉴가 수정된다.
 
 // TODO 메뉴 삭제
 // - [ ] 메뉴 살제 버튼 클릭 이벤트를 받고, 메뉴 삭제 컨펌 모달창이 뜬다. (브라우저에서 제공하는 confirm 인터페이스를 활용)
